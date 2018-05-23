9d116fd6806828d8e5ba58efaec8de9324d35b7f
Gumball1506 <38982972+Gumball1506@users.noreply.github.com>
2018-05-23 09:14:07 +0700

    Update and rename lineage.mk to liquid.mk

--- liquid.mk
+++ liquid.mk
@@ -5,8 +5,11 @@ PRODUCT_RELEASE_NAME := Samsung Galaxy Grand Prime
 TARGET_SCREEN_WIDTH := 540
 TARGET_SCREEN_HEIGHT := 960
 
+#Bootanimation Res
+TARGET_BOOT_ANIMATION_RES := 540
+
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
